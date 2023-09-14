71b5a3e6b81948b2afdb327f4733854f785f48f0
Zihlu Wang <really@zihlu.wang>
2023-09-14 16:53:30 +0800

    refactor: Convert Kotlin back to Java

---
+++ src/main/java/wang/zihlu/security/constant/CommonValues.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2023. Zihlu Wang
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+package wang.zihlu.security.constant;
+
+/**
+ * CommonValues
+ *
+ * @author Zihlu Wang
+ * @since 12 Sept, 2023
+ */
+public final class CommonValues {
+    public static final String JSON_WITH_CHARSET = "application/json; charset=UTF-8";
+}
---
+++ src/main/java/wang/zihlu/security/constant/ResponseHeaders.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2023. Zihlu Wang
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+package wang.zihlu.security.constant;
+
+/**
+ * ResponseHeaders
+ *
+ * @author Zihlu Wang
+ * @since 12 Sept, 2023
+ */
+public final class ResponseHeaders {
+    public static final String BIZ_ERROR = "X-Biz-Error";
+
+    public static final String BIZ_ERROR_CODE = "X-Biz-Error-Code";
+}
